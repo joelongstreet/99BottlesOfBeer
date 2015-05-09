87e856db6c8c5fbbc51a7b57f2803c3f564cde2e
Joe Longstreet <Longstreet@new-host.home>
2015-05-09 13:53:00 -0500

    add alcohol sensor and button press detection

--- _99BottlesOfBeer/_99BottlesOfBeer.ino
+++ _99BottlesOfBeer/_99BottlesOfBeer.ino
@@ -1,6 +1,10 @@
 // configuration options
 int ledSpeed    = 50;
 
+// alcohol capturing
+int alcoholSensor = A0;
+int captureButton = 8;
+
 // first shift register (A)
 int aLatchPin   = 3;
 int aClockPin   = 4;
@@ -15,7 +19,10 @@ byte bLeds      = 0;
 
 
 void setup() 
-{
+{ 
+  // establish button as an input
+  pinMode(captureButton, INPUT);
+
   // establish registers as outputs 
   pinMode(aLatchPin, OUTPUT);
   pinMode(aDataPin,  OUTPUT);  
@@ -28,12 +35,31 @@ void setup()
 
 void loop() 
 {
-  // test mode
-  for(int i=0; i<=16; i++){
-    lightLEDs(i);
-    delay(1000);
+  updateShiftRegisterA();
+  updateShiftRegisterB();
+  
+  // when the button is pressed...
+  if(digitalRead(captureButton)){
+    // capture 100 alcohol values over the course of a
+    // single second and report the greatest value
+    int maxAlcoholValue = 0;
+    for(int i=0; i<100; i++){
+      int alcoholValue = analogRead(alcoholSensor);
+      if(alcoholValue > maxAlcoholValue){
+        maxAlcoholValue = alcoholValue;
+      }
+      delay(10);
+    }
+    
+    // take the max alcohol value and map from potential low/high
+    // to number of LEDs. Then light
+    int ledCount = map(maxAlcoholValue, 5, 40, 0, 16);
+    if(ledCount < 0) ledCount = 1;
+    lightLEDs(ledCount);
+    
+    delay(2000);
+    lightLEDs(0);
   }
-  delay(2000);
 }
 
 
@@ -60,7 +86,7 @@ void lightLEDs(int ledCount){
   for (int i = 0; i < countB; i++)
   {
     bitSet(bLeds, i);
-    updateShiftRegisterA();
+    updateShiftRegisterB();
     delay(ledSpeed);
   }
 }
